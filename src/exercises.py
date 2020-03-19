d85498557a3f903d6b0f8fedbc53135256fb9898
Agastyabisht123 <agastya.bisht@datagrokr.com>
2020-03-19 11:56:16 +0530

    Added features in program

--- src/exercises.py
+++ src/exercises.py
@@ -1,11 +1,5 @@
 def reverse_list(input_list):
-    """
-    Reverses order of elements in list input_list.
-    """
-    return None
+    return list(reversed(input_list))
 
 def count_digits(number):
-    """
-    Return count of digits
-    """
-    return None
+    return len(str(number))
